f1d97b114667fbf15dcfb27d4ee3ad12962ed4d0
blazingjove <blazingjove@gmail.com>
2025-01-22 12:17:16 -0600

    -added database connection

--- src/main/main.java
+++ src/main/main.java
@@ -5,16 +5,30 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import main.JDBC;
+import java.util.Locale;
 
+
+/** main class that creates an application.*/
 public class main extends Application {
     @Override
+
+    /**Initializes the mainView.fxml.
+     * @param stage
+     * throws Exception*/
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/resource/view/mainView.fxml"));
         stage.setTitle("First Screen");
-        stage.setScene(new Scene(root, 800,600));
+        stage.setScene(new Scene(root, 400  ,350));
         stage.show();
     }
+
+    /** main method that starts FXML and connects to database.
+     *
+     * @param args */
     public static void main(String[] args) {
+        JDBC.openConnection();
         launch(args);
+        JDBC.closeConnection();
     }
 }
