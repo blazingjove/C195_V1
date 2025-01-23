458107c0fee2752d52a7ec26464f057acc39f40b
blazingjove <blazingjove@gmail.com>
2025-01-23 11:01:11 -0600

    -commit before moving some items around v2

--- src/main/main.java
+++ src/main/main.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import java.io.IOException;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 
@@ -14,13 +15,11 @@ import java.util.ResourceBundle;
 public class main extends Application {
 
     /** Initializes the mainView.fxml.
-     * @param stage
-     * @throws IOException
-     * */
+     * @param stage sets the login view to define starting parameters */
     @Override
     public void start(Stage stage) throws IOException {
         ResourceBundle resourceBundle = ResourceBundle.getBundle("language/login", Locale.getDefault());
-        Parent root = FXMLLoader.load(getClass().getResource("/resource/view/loginView.fxml"), resourceBundle);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/resource/view/loginView.fxml")), resourceBundle);
         stage.setTitle(resourceBundle.getString("title"));
         stage.setScene(new Scene(root, 400  ,320));
         stage.show();
@@ -28,11 +27,10 @@ public class main extends Application {
 
     /** main method that starts FXML and connects to database.
      * commented out code is to change locale for troubleshooting.
-     * @param args*/
+     * @param args launches main*/
     public static void main(String[] args) {
         //Locale.setDefault(new Locale("fr")); //sets system language to French
-        System.out.println("Locale set to: " + Locale.getDefault());
-
+        //System.out.println("language set to: " + Locale.getDefault());
         JDBC.openConnection();
         launch(args);
         JDBC.closeConnection();
